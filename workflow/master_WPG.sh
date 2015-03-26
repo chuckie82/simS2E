3f1b99c682bfb795d3ba40e2cd14e210acd809aa
Chuck <chun.hong.yoon@desy.de>
2015-03-26 14:50:00 +0100

    changes needed for sim_example

--- workflow/master_WPG.sh
+++ workflow/master_WPG.sh
@@ -4,8 +4,8 @@
 	#
     echo "Welcome to prop"
     cd $ROOT ;  pwd
-    JMAX=NUM_PROP_OUT;
-    myInd=$(printf %07d $JMAX)
+    #JMAX=NUM_PROP_OUT;
+    #myInd=$(printf %07d $JMAX)
     cd $ROOT/modules/prop
-    /afs/desy.de/group/exfel/software/karabo-trunk/extern/bin/python $ROOT/modules/prop/propagateSE.py --input-directory $ROOT/data/$PROJECT/FELsource --output-directory $ROOT/data/$PROJECT/prop --cpu-number 8
-    /afs/desy.de/group/exfel/software/karabo-trunk/extern/bin/python $ROOT/modules/prop/diagnostics.py --input-file $ROOT/data/$PROJECT/prop/prop_out_$myInd
+    python $ROOT/modules/prop/propagateSE.py --input-directory $ROOT/data/$PROJECT/FELsource --output-directory $ROOT/data/$PROJECT/prop --cpu-number 8
+    #/afs/desy.de/group/exfel/software/karabo-trunk/extern/bin/python $ROOT/modules/prop/diagnostics.py --input-file $ROOT/data/$PROJECT/prop/prop_out_$myInd
