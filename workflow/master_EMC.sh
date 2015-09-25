c1333246f5885fd06eb90fb0b228c35fc0c8fb13
Chuck <chun.hong.yoon@desy.de>
2015-09-25 10:12:36 +0200

    fixed emc scripts

--- workflow/master_EMC.sh
+++ workflow/master_EMC.sh
@@ -9,9 +9,23 @@ cd ${SRC}
 
 #Let's parse some options.
 OPTS="-s ${SRC} -T ${TMP} -i ${INPUT} -o ${OUTPUT} -q ${INITIALQUAT} -Q ${MAXQUAT} -m ${MAXITER} -e ${MINERR} "
+
 ADDOPT=" "
-[$PLOT -eq 1] && {ADDOPT="${ADDOPT} -p"} 
-[$DETAILED -eq 1] && {ADDOPT="${ADDOPT} -d"}
+if [ $BEAMSTOP -eq 1 ] 
+then
+	ADDOPT="${ADDOPT} -b" 
+fi
+
+if [ $PLOT -eq 1 ] 
+then
+	ADDOPT="${ADDOPT} -p"
+fi
+
+if [ $DETAILED -eq 1 ] 
+then
+	ADDOPT="${ADDOPT} -d"
+fi
+
 OPTS="${OPTS} ${ADDOPT}"
 
 #Starts multiple reconstructions 
@@ -20,6 +34,7 @@ do
     echo "Starting reconstruction ${i}."
     echo "Temporary output stored in ${TMP}."
     echo "Output stored in ${OUTPUT}."
-	nohup python runEMC.py ${OPTS} &
+	echo python runEMC.py ${OPTS} &
+	python runEMC.py ${OPTS} &
 	sleep ${SLEEPDUR}
 done
