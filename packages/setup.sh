c5280ae2fc008ca2d49062c46d475eb0a1ed16bf
Chuck <chun.hong.yoon@desy.de>
2015-10-04 21:19:33 +0200

    typo

--- packages/setup.sh
+++ packages/setup.sh
@@ -3,26 +3,26 @@ PWD=`pwd`
 echo $PWD
 basepath=$PWD
 
-echo "Buiding fast"
+echo "Building fast"
 cd "$basepath/fast"
 docker build -t fast:v0.1 .
 
-echo "Buiding wpg"
+echo "Building wpg"
 cd "$basepath/wpg"
 docker build -t wpg:v0.1 .
 
-echo "Buiding pmi_demo"
+echo "Building pmi_demo"
 cd "$basepath/pmi_demo"
-docker build -t pmi:v0.1 .
+docker build -t pmi_demo:v0.1 .
 
-echo "Buiding singfel"
+echo "Building singfel"
 cd "$basepath/singfel"
 docker build -t singfel:v0.1 .
 
-echo "Buiding EMC"
+echo "Building EMC"
 cd "$basepath/s2e_recon/EMC_Src"
 docker build -t emc:v0.1 .
 
-echo "Buiding DM"
+echo "Building DM"
 cd "$basepath/s2e_recon/DM_Src"
 docker build -t dm:v0.1 .
