dc4798eaff292de81b9f945e9570cd19749e4052
Chuck <chun.hong.yoon@desy.de>
2015-10-10 11:33:03 +0200

    revert setup.sh to v0.1

--- packages/setup.sh
+++ packages/setup.sh
@@ -5,24 +5,24 @@ basepath=$PWD
 
 echo "Building fast"
 cd "$basepath/fast"
-docker build -t fast:v0.2 .
+docker build -t fast:v0.1 .
 
 echo "Building wpg"
 cd "$basepath/wpg"
-docker build -t wpg:v0.2 .
+docker build -t wpg:v0.1 .
 
 echo "Building pmi_demo"
 cd "$basepath/pmi_demo"
-docker build -t pmi_demo:v0.2 .
+docker build -t pmi_demo:v0.1 .
 
 echo "Building singfel"
 cd "$basepath/singfel"
-docker build -t singfel:v0.2 .
+docker build -t singfel:v0.1 .
 
 echo "Building EMC"
 cd "$basepath/emc"
-docker build -t emc:v0.2 .
+docker build -t emc:v0.1 .
 
 echo "Building DM"
 cd "$basepath/dm"
-docker build -t dm:v0.2 .
+docker build -t dm:v0.1 .
