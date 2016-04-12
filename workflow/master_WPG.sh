4bb66cc6aa2811acee3a57a014c391146c03dba9
Alexey Buzmakov <buzmakov@gmail.com>
2016-04-12 19:34:52 +0300

    move prop module inside WPG package

--- workflow/master_WPG.sh
+++ workflow/master_WPG.sh
@@ -7,6 +7,11 @@
     cd $WORKDIR ;  pwd
     #JMAX=NUM_PROP_OUT;
     #myInd=$(printf %07d $JMAX)
-    cd $WORKDIR/modules/prop
-    python $WORKDIR/modules/prop/propagateSE.py --input-directory $ROOT/data/$PROJECT/FELsource --output-directory $ROOT/data/$PROJECT/prop --cpu-number 1
+    
+    cd $WORKDIR/packages/WPG/s2e/prop
+    python $WORKDIR/packages/WPG/s2e/prop/propagateSE.py --input-directory $ROOT/data/$PROJECT/FELsource --output-directory $ROOT/data/$PROJECT/prop --cpu-number 1
+    
+    # cd $WORKDIR/modules/prop
+    # python $WORKDIR/modules/prop/propagateSE.py --input-directory $ROOT/data/$PROJECT/FELsource --output-directory $ROOT/data/$PROJECT/prop --cpu-number 1
+    
     #/afs/desy.de/group/exfel/software/karabo-trunk/extern/bin/python $ROOT/modules/prop/diagnostics.py --input-file $ROOT/data/$PROJECT/prop/prop_out_$myInd
